617810fe97d895781b4d39222ed46216bf1d1c8b
ryohei_akagi <abbbbbbbb35@gmail.com>
2023-11-08 16:08:50 +0900

    P277までの内容 スロット 次回v-slotを使った名前付きスロット

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -3,11 +3,10 @@
     <h1>{{ title }}</h1>
     <pre>{{ message }}</pre>
     <hr>
-    <div class="area"
-    v-on:click.left.prevent="left"
-    v-on:click.middle.prevent="middle"
-    v-on:click.right.prevent="right">
-      click here!
+    <div class="alert alert-light">
+      <h3>Inner Slot</h3>
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -16,54 +15,11 @@ export default{
   name:'HelloWorld',
   data:function(){
     return{
-      title:'Event',
-      message:'',
-    }
-  },
-  methods:{
-    // eventの中にはkeyboadeventというものが入ってる
-    left(){
-      this.message = '[left button]'
-    },
-    right(){
-      this.message = '[right button]'
-    },
-    middle(){
-      this.message = '[middle button]'
+      title:'Slot',
+      message:'This is message.',
     }
   },
 }
 </script>
 
-<style>
-pre{
-  font-size:16pt;
-  line-height:1.25;
-}
-div.out{
-  padding:0px;
-  background-color:#eee;
-  width:300px;
-  height:200px;
-}
-div.mid{
-  padding:0px;
-  background-color:#ddd;
-  width:200px;
-  height:170px;
-}
-div.in{
-  padding:0px;
-  background-color: #ccc;
-  width:100px;
-  height:140px;
-}
-.area{
-  width:300px;
-  height:100px;
-  background-color: #fff;
-  padding:10px;
-  font-size:20pt;
-}
-</style>
 

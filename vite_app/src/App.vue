95c0d091f4341c7dc6aa93dc7778938a44f35092
ryohei_akagi <abbbbbbbb35@gmail.com>
2023-11-08 17:12:59 +0900

    P280 スロットに値を設定するはまだしていない

--- vite_app/src/App.vue
+++ vite_app/src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id = "app">
-    <HelloWorld title="slot">
-      <p>***this is defalut text***</p>
+    <HelloWorld >
+      <p>Begin!...</p>
+      <template v-slot:first>
+        ***First message***
+      </template>
+
+      <p>..Middle..</p>
+      <template v-slot:second>
+        ***Second message***
+      </template>
+      
+      <p>..End</p>
+      <template v-slot:third>
+        ***Third message***
+      </template>
     </HelloWorld> 
   </div>
 </template> 

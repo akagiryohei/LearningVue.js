e70c2adcd144389d37191e7249074ffc81be8c2c
ryohei_akagi <abbbbbbbb35@gmail.com>
2023-11-09 13:49:33 +0900

    P292の前々のコード

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
     <p>{{ message }}</p>
     <button class="btn btn-primary m-3" v-on:click="doAction">{{ btn }}</button>
     <transition name="transit">
-      <p v-if="flg" class="alert alert-light h5">
+      <p v-if="flg" class="alert p-3 h5 trans">
         Transition 
       </p>
     </transition>
@@ -29,5 +29,35 @@ export default{
   },
 }
 </script>
+
+<style>
+.trans{
+  background-color: black;
+  color:white;
+  padding:10px;
+  font-size:20pt;
+}
+.transit-enter-active{
+  transition:opacity 0.5s;
+}
+.transit-leave-active{
+  transition: opacity 5.0s;
+}
+.transit-enter{
+  opacity: 0;
+}
+.transit-enter-from{
+  opacity:0;
+}
+.transit-enter-to{
+  opacity: 1.0;
+}
+.transit-leave{
+  opacity: 1.0;
+}
+.transit-leave-to{
+  opacity: 0;
+}
+</style>
 
 

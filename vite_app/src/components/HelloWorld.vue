e2d6276c7ebb22cfda1f70a1505327dca2dd10dd
ryohei_akagi <abbbbbbbb35@gmail.com>
2023-11-09 16:27:35 +0900

    P301のジグザクの前まで

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -1,16 +1,11 @@
 <template>
-  <div class="hello">
+  <div class="alert alert-primary">
     <h1>{{title}}</h1>
     <p>{{message}}</p>
-    <hr/>
-    <button v-on:click="doAction">
-    {{btn}}
+    <button class="btn btn-primary mb-3" v-on:click="doAction">
+    Show/Hide
     </button>
-    <transition name="transit" 
-        v-on:before-enter="startAction"
-        v-on:before-leave="startAction"
-        v-on:after-enter="endAction" 
-        v-on:after-leave="endAction">
+    <transition name="transit" >
       <p v-if="flg" class="trans">Transition!</p>
     </transition>
   </div>
@@ -19,36 +14,18 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    title:String,
-  },
   data: function() {
     return {
+      title:'Trans&Anim',
       message:'Transition Sample!',
       flg:true,
       btn:'Hide',
     }
   },
   methods:{
-    doAction: function(){
-      this.flg = !this.flg
+    doAction(){
+      this.flg = !this.flg;
     },
-    startAction: function(){
-      if (this.flg){
-        this.message = '現れます……'
-      } else {
-        this.message = '消えます……'
-      }
-    },
-    endAction: function(){
-      if (this.flg){
-        this.btn = 'Hide'
-        this.message = '現れました。'
-      } else {
-        this.btn = 'Show'
-        this.message = '消えました。'
-      }
-    }
   },
 }
 </script>
@@ -62,25 +39,15 @@ export default {
   font-size:20pt;
 }
 .transit-enter-active{
-  transition:opacity 0.5s;
+  transition: 1.0s;
 }
 .transit-leave-active{
-  transition: opacity 5.0s;
+  transition: 1.0s;
+  opacity: 0.5;
 }
-.transit-enter{
-  opacity: 0;
-}
-.transit-enter-from{
-  opacity:0;
-}
-.transit-enter-to{
-  opacity: 1.0;
-}
-.transit-leave{
-  opacity: 1.0;
-}
-.transit-leave-to{
-  opacity: 0;
+.transit-enter, .transit-leave-to{
+  transform:rotateZ(360deg);
+  opacity:0.1;
 }
 </style>
 

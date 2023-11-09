a7a52be36657bf61541970536b5cfa9e7b4821e7
ryohei_akagi <abbbbbbbb35@gmail.com>
2023-11-09 15:19:39 +0900

    P295までの内容

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -1,34 +1,58 @@
 <template>
-  <div class="alert alert-primary">
-    <h2>{{ title }}</h2>
-    <p>{{ message }}</p>
-    <button class="btn btn-primary m-3" v-on:click="doAction">{{ btn }}</button>
-    <transition name="transit">
-      <p v-if="flg" class="alert p-3 h5 trans">
-        Transition 
-      </p>
+  <div class="hello">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <hr/>
+    <button v-on:click="doAction">
+    {{btn}}
+    </button>
+    <transition name="transit" 
+        v-on:before-enter="startAction"
+        v-on:before-leave="startAction"
+        v-on:after-enter="endAction" 
+        v-on:after-leave="endAction">
+      <p v-if="flg" class="trans">Transition!</p>
     </transition>
   </div>
 </template>
 
 <script>
-export default{
-  name:'HelloWorld',
-  data(){
-    return{
-      title:'Trans&Anim',
-      message:'This is Transition sample!',
+export default {
+  name: 'HelloWorld',
+  props: {
+    title:String,
+  },
+  data: function() {
+    return {
+      message:'Transition Sample!',
       flg:true,
-      btn:'Show/Hide',
+      btn:'Hide',
     }
   },
   methods:{
-    doAction(){
+    doAction: function(){
       this.flg = !this.flg
     },
+    startAction: function(){
+      if (this.flg){
+        this.message = '現れます……'
+      } else {
+        this.message = '消えます……'
+      }
+    },
+    endAction: function(){
+      if (this.flg){
+        this.btn = 'Hide'
+        this.message = '現れました。'
+      } else {
+        this.btn = 'Show'
+        this.message = '消えました。'
+      }
+    }
   },
 }
 </script>
+
 
 <style>
 .trans{

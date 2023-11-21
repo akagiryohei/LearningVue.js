e67efb5d9e405687dc3ca1ef801b7b011c6d6b33
ryohei_akagi <abbbbbbbb35@gmail.com>
2023-11-21 10:16:02 +0900

    P446の直前　 年齢の範囲を絞るのは次

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="alert alert-primary">
-    <h1>{{ data.title }}</h1>
-    <p>{{ data.message }}</p>
+    <h1>{{data.title}}</h1>
+    <p>{{data.message}}</p>
     <div class="form-inline my-2">
       <input type="text" v-model="data.find"
         class="form-control">
@@ -9,7 +9,7 @@
         Click</button>
     </div>
     <div class="alert alert-light">
-      {{ data.fire_data }}
+      {{data.fire_data}}
     </div>
   </section>
 </template>
@@ -18,24 +18,23 @@
 import axios from 'axios'
 import { onMounted, reactive } from 'vue'
 
-// let url = "https://jsonplaceholder.typicode.com/posts/"
-let url = "https://akigi-vue3-default-rtdb.firebaseio.com/person/"
+let url = "https://akigi-vue3-default-rtdb.firebaseio.com/person.json?orderBy=%22$key%22&equalTo=%22" //☆
 
 export default {
   setup(props) {
     const data = reactive({
-      title: 'Firebase',
-      message: 'This is Firebase sample.',
-      find:'',
+      title:'Firebase',
+      message:'This is Firebase sample.',
+      find: '',
       fire_data: {},
     })
-    const getData = () => {
-      let id_url = url + data.find + '.json'
+    const getData = ()=> {
+      let id_url = url + data.find + '%22'
       axios.get(id_url).then((result) => {
         data.message = 'get ID=' + data.find
-        if(result.data != null){
+        if (result.data != null) {
           data.fire_data = result.data
-        }else{
+        } else {
           data.fire_data = 'no data found...'
         }
       }).catch((error)=>{
@@ -43,7 +42,7 @@ export default {
         data.fire_data = {}
       })
     }
-    onMounted(() => {
+    onMounted(()=> {
       getData()
     })
     return { data, getData }

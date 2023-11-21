7464c6d9da49f7a593054ac7aedb63ffac158a72
ryohei_akagi <abbbbbbbb35@gmail.com>
2023-11-21 10:57:02 +0900

    P448まで 年齢の範囲で検索

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -1,50 +1,51 @@
 <template>
   <section class="alert alert-primary">
-    <h1>{{data.title}}</h1>
-    <p>{{data.message}}</p>
+    <h1>{{ data.title }}</h1>
+    <p>{{ data.message }}</p>
     <div class="form-inline my-2">
-      <input type="text" v-model="data.find"
-        class="form-control">
+      <input type="text" v-model="data.find" class="form-control">
       <button @click="getData" class="btn btn-primary">
         Click</button>
     </div>
-    <div class="alert alert-light">
-      {{data.fire_data}}
-    </div>
+    <ul v-for="(item, key) in data.fire_data" class="list-group">
+      <li class="list-group-item text-left">
+        <strong>{{ key }}</strong><br>{{ item }}
+      </li>
+    </ul>
   </section>
 </template>
 
 <script>
 import axios from 'axios'
-import { onMounted, reactive } from 'vue'
+import { reactive } from 'vue'
 
-let url = "https://akigi-vue3-default-rtdb.firebaseio.com/person.json?orderBy=%22$key%22&equalTo=%22" //☆
+let url = "https://akigi-vue3-default-rtdb.firebaseio.com/person.json?orderBy=%22age%22" //☆
 
 export default {
   setup(props) {
     const data = reactive({
-      title:'Firebase',
-      message:'This is Firebase sample.',
+      title: 'Firebase',
+      message: 'This is Firebase sample.',
       find: '',
       fire_data: {},
     })
-    const getData = ()=> {
-      let id_url = url + data.find + '%22'
-      axios.get(id_url).then((result) => {
+    const getData = () => {
+      // 配列にした
+      let range = data.find.split(',')
+      let age_url = url + "&startAt=" + range[0]
+        + "&endAt=" + range[1]
+      axios.get(age_url).then((result) => {
         data.message = 'get ID=' + data.find
         if (result.data != null) {
           data.fire_data = result.data
         } else {
           data.fire_data = 'no data found...'
         }
-      }).catch((error)=>{
+      }).catch((error) => {
         data.message = 'ERROR!'
         data.fire_data = {}
       })
     }
-    onMounted(()=> {
-      getData()
-    })
     return { data, getData }
   },
 }
